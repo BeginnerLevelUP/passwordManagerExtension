7952d4ee8dfd2db4a16f5dea98014b3f21c3e6be
Damion Morgan <only4school5680@gmail.com>
2024-01-21 16:42:10 -0500

    messed up starting over we move

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import './App.css';
-import Profile from './components';
+
 
 function App() {
   return (
-   <Profile></Profile>
+<h1>hello</h1>
   );
 }
 
 export default App;
 
-//  const getBearerToken=()=>{
-//    const token= localStorage.getItem('id_token');
-//    console.log(token)
-//  }
 
-// chrome.action.onClicked.addListener((tab) => {
-//   chrome.tabs.query({ url: 'https://passwordmanager-zep7.onrender.com/*' }, (tabs) => {
-//     const page = tabs[0];
-//     if (page) {
-//       console.log('Page already open', page);
-//       chrome.tabs.update(page.id, { active: true });
-//       checkLoginStatus();
-//     } else {
-//       console.log('Opening New Page');
-//       chrome.tabs.create({
-//         url: 'https://passwordmanager-zep7.onrender.com/',
-//       });
-//     }
-//   });
-// });
