ed87ea8a071aa2f93c05622770c87fb446407ba7
Damion Morgan <only4school5680@gmail.com>
2024-01-25 14:38:53 -0500

    back to the drugs back to the money back to the life

--- public/api/checkAccounts.js
+++ public/api/checkAccounts.js
@@ -1,13 +1,17 @@
  export const checkAccounts=async()=>{
  chrome.storage.local.get(['accountUrls'],(result)=>{
     console.log(result)
-    result.accountUrls.map(async(url)=>{
+    result.accountUrls.map(async(url,index)=>{
       const accountUrl=  await chrome.tabs.query({url:url})
       const page=accountUrl[0]
       if(page){
-        console.log(page)
+        console.log(`Account ${index}`,page)
+        if(page.active){
+          console.log('currently on page')
+           chrome.browserAction.openPopup();
+        }
       }else{
-        console.log('page not found')
+        console.log(`Account ${index} not found`)
       }
     })
   })
--- public/background.js
+++ public/background.js
@@ -1,14 +1,15 @@
 import {checkLoginStatus,getBearerKey} from "./fetchUser.js"
 import { checkAccounts } from "./api/checkAccounts.js"
 chrome.runtime.onInstalled.addListener(details=>{
-         checkAccounts() 
      setInterval(()=>{
+
 getBearerKey()
   chrome.storage.local.get(['bearerKey'], (result) => {
     const {bearerKey}=result
     checkLoginStatus(bearerKey)
   });
+          checkAccounts() 
      },1000)
-      
-
+   
 })  
+
